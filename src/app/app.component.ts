9d09f3fd61481ca36d9c85e0ef091c1c70298ea6
HyperExy <109340461+HyperExy@users.noreply.github.com>
2022-07-19 15:32:48 +0300

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+﻿import { Component, VERSION } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+
+@Component({
+    selector: 'my-app',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+    checkoutForm = this.formBuilder.group({
+        name: '',
+        email: '',
+        phone: '',
+        theme: '',
+        text: '',
+    });
+    constructor(
+        private formBuilder: FormBuilder,
+    ) { }
+    onSubmit(): void {
+        console.warn('Сообщение было отправлено', this.checkoutForm.value);
+        this.checkoutForm.reset();
+        Program.Post();
+    }
+}
